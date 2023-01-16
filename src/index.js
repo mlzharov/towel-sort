6754ee96b7bbb278195055b2f0f3335a04f946fc
Mikhail Zharov <m.zharov@gmail.com>
2023-01-16 07:59:14 +0300

    modified:   package-lock.json 	modified:   src/index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (arguments.length === 0 || matrix.length === 0) return [];
+
+  const matrixSort = matrix.reduce((accum, current, index) => {
+      if (index % 2) {current.reverse();}
+      return [...accum, ...current];
+  }, []);
+
+  return matrixSort;
 }
